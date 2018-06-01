a28d7dc0c9e904e591b05f84b535f7a6cd7bd238
David Jaquet <djaquet5@gmail.com>
2018-06-01 14:17:21 +0200

    Add express JS and generate some movies in JSON

--- README.md
+++ README.md
@@ -15,13 +15,19 @@ To test it :
 $ telnet <IP_address> 8080
 ```
 
-## Need to check
-
-[Configure Apache Web Server](https://www.digitalocean.com/community/tutorials/how-to-configure-the-apache-web-server-on-an-ubuntu-or-debian-vps)
-
 ## Step 2
 npm init -> package name = students
 	    version = 0.1.0
 	    decription = Step 2 of the HTTP lab
 
 Use of node 8.11 and the module Chance to get random value
+
+```bash
+$ docker build -t res/express_movies
+$ docker run res/express_movies
+```
+
+## Need to check
+
+[Configure Apache Web Server](https://www.digitalocean.com/community/tutorials/how-to-configure-the-apache-web-server-on-an-ubuntu-or-debian-vps)
+[Anatomy of an HTTP Transaction](https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/)
--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,54 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.send(generateMovies());
+});
+
+app.listen(3000, function(){
+	console.log("Accepting HTTP requests on port 3000.");
+});
+
+
+function generateMovies(){
+	var nbMovies = generateRandomInt(1, 10);
+	var movies = [];
+
+	for(var i = 0; i < nbMovies; i++){
+		movies.push({
+			name: chance.word({syllables: 3}),
+			plot: chance.sentence(),
+			description: chance.paragraph(),
+			duration: generateRandomInt(80, 200),
+			actors: generateActors()
+		});
+	}
+
+	console.log(movies);
+	return movies;
+}
+
+function generateActors(){
+	var nbActors = generateRandomInt(1, 5);
+	var actors = [];
+	
+	for(var i = 0; i < nbActors; i++){
+		actors.push({
+			firstName: chance.first(),
+			lastName: chance.last(),
+			age: chance.age({type: 'adult'})
+		});
+	}
+	
+	return actors;
+}
+
+function generateRandomInt(minValue, maxValue){
+	return chance.integer({
+		min: minValue,
+		max: maxValue
+	});
+}
